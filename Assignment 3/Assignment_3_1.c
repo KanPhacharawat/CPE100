05f05f418e3c7101682ca55dca4ca31a36a405d5
KanPhacharawat <83383938+KanPhacharawat@users.noreply.github.com>
2024-08-23 15:04:52 +0700

    Assignment 3

---
+++ Assignment 3/Assignment_3_1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    int type;
+    char product[100];
+    float money, change;
+    int isvalid = 1;
+
+    scanf("%d %f", &type, &money);
+
+    if(type == 1){
+        change = money - 7.0;
+        strcpy(product, "Water");
+    }else if(type == 2){
+        change = money - 13.0;
+        strcpy(product, "Soda");
+    }else if(type == 3){
+        change = money - 20.0;
+        strcpy(product, "Juice");
+    }
+
+    if(type < 1 || type > 3){
+        printf("Invalid choice!");
+        isvalid = 0;
+    }
+    if(change >= 0.0 && isvalid){
+        printf("Dispensing your drink: %s\n", product);
+    }if(change > 0.0 && isvalid){
+        printf("Here is your change: %.2f", change);
+    }if(change == 0 && isvalid){
+        printf("No change");
+    }if(change < 0 && isvalid){
+        printf("Insufficient funds");
+    }
+
+    return 0;
+}
---
+++ Assignment 3/Assignment_3_2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    float price;
+    int ispromo;
+    int type;
+
+    scanf("%f %d %d", &price, &ispromo, &type);
+
+    if(ispromo){
+        if(type == 1 || type == 2){
+            if(price >= 5000)   price = price * .75;
+            else if(price >= 2000) price = price * .85;
+            else price = price * .9;
+        }else if(type == 3){
+            if(price >= 5000)   price = price * .8;
+            else if(price >= 2000) price = price * .9;
+            else price = price * .95;
+        }
+    }
+
+    price = price * 1.07;
+
+    printf("Net price after discount and tax: %.2f Baht", price);
+
+    return 0;
+}
